49bf020ff25b174e004e7fe795c554dc144b3d43
Kireobat <erik.flatabo@gmail.com>
2022-12-07 12:31:14 +0100

    Is now able to scrape

--- src/priceFetcher.js
+++ src/priceFetcher.js
@@ -21,27 +21,50 @@ let tromsoPriceToday = document.querySelector('td[class="sortable"]:nth-of-type(
 // Velger riktig rute for å hente ut pris
 // document.querySelector('td[class="sortable"]:nth-of-type(x)').innerText
 
+function delay(time) {
+    return new Promise(function(resolve) { 
+        setTimeout(resolve, time)
+        console.log("waiting for " + time + "ms");
+    });
+}
+
 // Fetches the price from Nordpool
 
 async function getPrice() {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto(url);
+    await delay(3000);
+    await page.screenshot({path: 'example.png'});
 
 
     /*
     osloPriceTodayText = await page.$("td[class='sortable']:nth-of-type(2)");
     osloPriceToday = await (await osloPriceTodayText.getProperty('innerText')).jsonValue();
     */
-
+    /*
     const value = await page.evaluate(
-        () => document.querySelectorAll("td[class='sortable']")[0].innerHTML
+        () => document.querySelectorAll("td[class='sortable']")[1].innerText
     );
+    */
 
+    /*
+    let scrapedData = await page.evaluate(() => {
+        Array.from(document.querySelectorAll("td[class='sortable']")[0]).map((element => ({
+            price: element.innerText
+        })))
+    });
+    */
 
-    pricesList.push(value);
-    console.log("prisliste1",pricesList);
-    console.log("oslopris",value);
+    const osloSelector = 'td[class="sortable"]:nth-of-type(2)'
+
+    const scrapedData = await page.$eval(osloSelector, (element) => {
+        return element.textContent;
+    })
+    //pricesList.push(value);
+    console.log("scraped data",scrapedData);
+    //console.log("oslopris",value);
+    await page.close();
     await browser.close();
     return pricesList;
 }
